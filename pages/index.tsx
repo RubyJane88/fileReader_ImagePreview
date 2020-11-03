f40f057b82bbfa76baf9159d2452e41e7e3519c2
RJ Cabagnot <rjcabagnot@hotmail.com>
2020-11-03 20:34:43 +0100

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,60 @@
+import React, { useRef, useState, useEffect } from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [image, setImage] = useState<File>();
+  const [preview, setPreview] = useState<string>();
+  const fileInputRef = useRef<HTMLInputElement>();
+
+  useEffect(() => {
+    if (image) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+      reader.readAsDataURL(image);
+    } else {
+      setPreview(null);
+    }
+  }, [image]);
+
+  return (
+    <div className={styles.container}>
+      <form>
+        {preview ? (
+          <img
+            src={preview}
+            alt={"preview"}
+            onClick={() => {
+              setImage(null);
+            }}
+          />
+        ) : (
+          <button
+            onClick={(event) => {
+              event.preventDefault();
+              fileInputRef.current.click();
+            }}
+          >
+            Add Image
+          </button>
+        )}
+
+        <input
+          type="file"
+          style={{ display: "none" }}
+          ref={fileInputRef}
+          accept="image/*"
+          onChange={(event) => {
+            const file = event.target.files[0];
+            if (file && file.type.substr(0, 5) === "image") {
+              setImage(file);
+            } else {
+              setImage(null);
+            }
+          }}
+        />
+      </form>
+    </div>
+  );
+}
